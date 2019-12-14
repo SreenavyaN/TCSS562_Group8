64e03afc3df509f4831a04003fe96ab25665952f
deekshagorige24 <58839032+deekshagorige24@users.noreply.github.com>
2019-12-14 13:09:09 -0800

    Delete callS3Query.sh

--- test/callS3Query.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Invoking Lambda function using AWS CLI"
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name "S3_Query" --region us-east-1 /dev/stdout | head -n 1 | head -c -2 ; echo`
-echo ""
-echo "AWS CLI RESULT:"
-echo $output | jq
-echo ""
-
